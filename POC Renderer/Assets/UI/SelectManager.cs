e16eb79ee886be9fb2f76e7750e3e2258bb2ec53
Pranav Manral <pranavmanral123@gmail.com>
2021-09-08 17:30:47 +1000

    Navigation

--- POC Renderer/Assets/UI/SelectManager.cs
+++ POC Renderer/Assets/UI/SelectManager.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class SelectManager : MonoBehaviour
 {
+    public int opt;
+
     public GameObject EnableModels;
     public static bool ModelDisabled = true;
 
     public GameObject EnableMaterials;
     public static bool MatDisabled = true;
+
+    public GameObject EnableAdd;
+    public static bool AddDisabled = true;
+
+    public GameObject EnableAdd2;
+    public static bool AddDisabled2 = true;
+
+    public GameObject SphereAdded;
+    public static bool SphereDisabled = true;
+    public Transform theLocation;
+
+    public GameObject optloc1;
+    public static bool optlocDisabled = true;
+
 
 
     void Start()
@@ -34,6 +50,55 @@ public class SelectManager : MonoBehaviour
             EnableModels.SetActive(false);
     }
 
+    public void Optloc()
+    {
+        if (optlocDisabled)
+
+            optloc1.SetActive(true);
+
+        else
+            optloc1.SetActive(false);
+    }
+
+    //public void AddSphere()
+   // {
+      //  opt++;
+
+       // if (opt == 1)
+       // {
+        //    Instantiate(SphereAdded, theLocation.position, Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+       // }
+   // }
+
+    public void AddSphere()
+    {
+        opt++;
+
+        if (opt == 1)
+        {
+            if (SphereDisabled)
+
+            {
+                SphereAdded.SetActive(true);
+                //SphereAdded.transform.SetParent(theLocation, true);
+                SphereAdded.transform.position = theLocation.position;
+            }
+
+            else
+                SphereAdded.SetActive(false);
+        }
+    }
+
+    public void CloseMod()
+    {
+        if (ModelDisabled)
+
+            EnableModels.SetActive(false);
+
+        else
+            EnableModels.SetActive(true);
+    }
+
     public void OpenMat()
     {
         if (MatDisabled)
@@ -42,5 +107,53 @@ public class SelectManager : MonoBehaviour
 
         else
             EnableMaterials.SetActive(false);
+    }
+
+    public void CloseMat()
+    {
+        if (MatDisabled)
+
+            EnableMaterials.SetActive(false);
+
+        else
+            EnableMaterials.SetActive(true);
+    }
+
+    public void AddModels1()
+    {
+        if (AddDisabled)
+
+            EnableAdd.SetActive(true);
+
+        else
+            EnableAdd.SetActive(false);
+    }
+    public void CloseAddOp()
+    {
+        if (AddDisabled)
+
+            EnableAdd.SetActive(false);
+
+        else
+            EnableAdd.SetActive(true);
+    }
+
+    public void AddModels2()
+    {
+        if (AddDisabled2)
+
+            EnableAdd2.SetActive(true);
+
+        else
+            EnableAdd2.SetActive(false);
+    }
+    public void CloseAddOp2()
+    {
+        if (AddDisabled2)
+
+            EnableAdd2.SetActive(false);
+
+        else
+            EnableAdd2.SetActive(true);
     }
 }
